c5465f8e07a2d8c04c6276587c7286337c9c5014
omibno <raihanwirasenop@gmail.com>
2024-05-14 18:35:48 +0700

    Signed-off-by: omibno <raihanwirasenop@gmail.com>
    	modified:   ChessEngine.py
    	modified:   Main.py
    	new file:   __pycache__/ChessEngine.cpython-312.pyc

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+"""
+File utama
+"""
 import pygame as p
 import ChessEngine
 
@@ -21,10 +24,29 @@ def main():
     gs = ChessEngine.GameState()
     loadimages()
     running = True
+    sqSelected = () #untuk mengetahui klik terbaru
+    playerClicks = [] #untuk mengetahui clicks, seperti click history
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() #untuk deteksi cursor
+                col1 = location[0]//SQ_SIZE
+                row1 = location[1]//SQ_SIZE
+                if sqSelected == (col1, row1): #klik kotak yg sama 2x
+                    sqSelected = () #deselect
+                    playerClicks = [] #untuk clear klik
+                else:
+                    sqSelected = (row1, col1)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = () #reset user click
+                    playerClicks = []
+
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -35,19 +57,19 @@ def drawGameState(screen, gs):
 
 def drawBoard(screen):
     colors = [p.Color('white'), p.Color('gray')]
-    for baris in range(DIMENSI):
-        for kolom in range(DIMENSI):
-            color = colors[((baris+kolom) % 2)]
-            p.draw.rect(screen, color, p.Rect(kolom*SQ_SIZE, baris*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+    for row in range(DIMENSI):
+        for col in range(DIMENSI):
+            color = colors[((row+col) % 2)]
+            p.draw.rect(screen, color, p.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 def drawPieces(screen, board):
-    for baris in range(DIMENSI):
-        for kolom in range(DIMENSI):
-            piece = board[baris][kolom]
+    for row in range(DIMENSI):
+        for col in range(DIMENSI):
+            piece = board[row][col]
             if piece != '--': #ada pion
                 #blit untuk menggambar pada screen(memasukkan pion)
-                screen.blit(IMAGES[piece], p.Rect(kolom*SQ_SIZE, baris*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                screen.blit(IMAGES[piece], p.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
