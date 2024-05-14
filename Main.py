af2634d0864fdc21676d3fdcbe07c7de56225254
omibno <raihanwirasenop@gmail.com>
2024-05-14 15:33:14 +0700

    Signed-off-by: omibno <raihanwirasenop@gmail.com>
    	modified:   Main.py

--- Main.py
+++ Main.py
@@ -10,4 +10,14 @@ IMAGES = {}
 def loadimages():
     pieces = ['bB', 'bK', 'bN', 'bp', 'bQ', 'bR', 'wB', 'wK', 'wN', 'wp', 'wQ', 'wR']
     for piece in pieces:
-        IMAGES[piece] = p.image.load('pieces/' + piece + '.png')
+        IMAGES[piece] = p.transform.scale(p.image.load('pieces/' + piece + '.png'), SQ_SIZE, SQ_SIZE)
+        #UNTUK MEMANGGIL BISA DENGAN IMAGES['wp]
+
+def main():
+    p.init()
+    screen = p.display.set_mode((LEBAR, TINGGI))
+    clock = p.time.Clock()
+    screen.fill(p.Color('white'))
+    GS = ChessEngine.GameState()
+    loadimages()
+    running = True
