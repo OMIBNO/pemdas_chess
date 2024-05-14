1368d31c7d815783d3be9613c4c8282521f0ce23
omibno <raihanwirasenop@gmail.com>
2024-05-14 13:34:08 +0700

    modified:   Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,12 @@
 import pygame as p
 import ChessEngine
+
+LEBAR = TINGGI = 512
+DIMENSI = 8
+SQ_SIZE = TINGGI // DIMENSI
+MAX_FPS = 15
+IMAGES = {}
+
+def loadimages():
+    pieces = ['bB', 'bK', 'bN', 'bp', 'bQ', 'bR', 'wB', 'wK', 'wN', 'wp', 'wQ', 'wR']
+    IMAGES[piece] = p.image.load('pieces/' + piece + '.png')
