8ccbcef9dba6884ccb6ba6b433efb73b98712486
omibno <raihanwirasenop@gmail.com>
2024-05-21 09:15:16 +0700

    Signed-off-by: omibno <raihanwirasenop@gmail.com>
    	modified:   ChessMain.py
    	new file:   SmartMoveFinder.py

--- ChessMain.py
+++ ChessMain.py
@@ -1,5 +1,5 @@
 import pygame as p
-import ChessEngine
+import ChessEngine, SmartMoveFinder
 
 LEBAR = 800 
 TINGGI = 512
@@ -28,12 +28,15 @@ def main():
     sqSelected = () #untuk mengetahui klik terbaru
     playerClicks = [] #untuk mengetahui clicks, seperti click history
     gameOver = False
+    playerOne = True #if a human is playing white, then this will be True. If an AI is playing, then
+    playerTwo = False #same as above but for black
     while running:
+        humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                if not gameOver:
+                if not gameOver and humanTurn:
                     location = p.mouse.get_pos()
                     col = location[0] // SQ_SIZE
                     row = location[1] // SQ_SIZE
@@ -68,6 +71,14 @@ def main():
                     playerClicks = []
                     moveMade = False
                     animate = False
+
+        #AI move finder
+        if not gameOver and not humanTurn:
+            AIMove = SmartMoveFinder.findRandomMove(validMoves)
+            gs.makeMove(AIMove)
+            moveMade = True
+            animate = True
+
 
         if moveMade:
             if animate:
@@ -190,7 +201,7 @@ def animateMove(move, screen, board, clock):
         #draw moving piece
         screen.blit(IMAGES[move.pieceMoved], p.Rect(col*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
         p.display.flip()
-        clock.tick(120)
+        clock.tick(60)
 
 def drawText(screen, text):
     font = p.font.SysFont('Inter', 32, True, False)
